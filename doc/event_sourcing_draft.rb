a5e3e5d789b9946106efdb3fcccc6cf776496b84
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2018-11-08 15:25:14 -0200

    Draft

--- doc/event_sourcing_draft.rb
+++ doc/event_sourcing_draft.rb
@@ -12,12 +12,37 @@ Buildings::Commands::ChangeBuildingAddress.new
 Buildings::CommandHandlers::CreateBuilding.new
 Buildings::CommandHandlers::UpdateBuilding.new
 Buildings::CommandHandlers::ChangeBuildingAddress.new
-
+Buildings::CommandHandlers::FindBuildingById.new
 
 Buildings::Queries::FindBuildingById.new
 Buildings::Queries::FindBuildingsByTerritoryId.new
 
-
 Buildings::Processors::NotifyPublisherOfAssignment.new
 
 Buildings::Projections::BuildingFinder.new
+
+
+
+
+
+
+
+
+
+
+class ChangeBuildingAddress
+  def handle(command)
+    unless validattor.valid?(command)
+      raise InvalidAddress
+    end
+
+    aggregate = finder.find(command.aggreagate_root)
+
+    event = AddressChanged.new(
+      street_name: command.street_name,
+      house_number: command.house_number
+    )
+
+    aggregate.apply(event)
+  end
+end
