c0df378330e77ed65f698e59dcf6e62aa680394f
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2018-11-10 00:05:53 -0200

    Configure code coverage

---
+++ spec/code_coverage.rb
@@ -0,0 +1,16 @@
+require 'simplecov'
+
+if ENV['TRAVIS']
+  require 'coveralls'
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+    [
+      SimpleCov::Formatter::HTMLFormatter,
+      Coveralls::SimpleCov::Formatter,
+    ]
+  )
+end
+
+SimpleCov.start do
+  add_filter '/spec'
+end
