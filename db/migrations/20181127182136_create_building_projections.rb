4a0173a4b6fd9497e68a1c2c8ea7760dbaf837ed
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2018-11-27 16:47:38 -0200

    Fix migration

--- db/migrations/20181127182136_create_building_projections.rb
+++ db/migrations/20181127182136_create_building_projections.rb
@@ -5,12 +5,13 @@ Hanami::Model.migration do
     create_table :projection_buildings do
       primary_key :id
 
-      column :uuid, String, null: false, unique: true, length: 36
-      column :address, String, null: false, unique: true
       column :number, String, null: false, unique: true
-      column :number_of_apartments, Integer, null: false, unique: true
-      column :name, String, null: false, unique: true
-      column :neighborhood, String, null: false, unique: true, index: true
+      column :uuid, String, null: false, unique: true, length: 36
+
+      column :name, String, null: true
+      column :address, String, null: false
+      column :number_of_apartments, Integer, null: true
+      column :neighborhood, String, null: true
 
       column :created_at, DateTime, null: false
       column :updated_at, DateTime, null: false
