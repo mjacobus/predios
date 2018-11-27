60c4b3e372806b0317c4b799ace6fe9b8490fdb2
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2018-11-27 17:18:27 -0200

    Fix test

--- spec/predios/domain/buildings/commands/create_building_spec.rb
+++ spec/predios/domain/buildings/commands/create_building_spec.rb
@@ -3,11 +3,11 @@
 require 'spec_helper'
 
 RSpec.describe Buildings::Commands::CreateBuilding do
-  let(:command) { described_class.new('foo' => 'bar') }
+  let(:command) { described_class.new('number' => 'the-number') }
 
   describe '#building_attributes' do
     it 'returns the building_attributes with symbol keys' do
-      expect(command.building_attributes).to eq foo: 'bar'
+      expect(command.building_attributes).to eq number: 'the-number'
     end
   end
 end
