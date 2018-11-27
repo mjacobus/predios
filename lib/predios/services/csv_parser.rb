13bd499182d459881e06daf195b707964f17a816
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2018-11-27 17:52:31 -0200

    Fix encoding for csv parser

--- lib/predios/services/csv_parser.rb
+++ lib/predios/services/csv_parser.rb
@@ -5,7 +5,7 @@ require 'csv'
 class CsvParser
   def parse_file(file)
     [].tap do |rows|
-      CSV.foreach(file, headers: true) do |row|
+      CSV.foreach(file, headers: true, encoding: 'utf-8') do |row|
         rows << DataBag.new(row.to_h)
       end
     end
