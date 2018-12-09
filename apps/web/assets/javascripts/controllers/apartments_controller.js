64ed5abe7d9f2b6ed49483358fd6f6dbc2c69fc0
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2018-12-09 15:48:56 -0200

    Small refactor

--- apps/web/assets/javascripts/controllers/apartments_controller.js
+++ apps/web/assets/javascripts/controllers/apartments_controller.js
@@ -1,22 +1,4 @@
-const apiRequest = (url, data, method = 'POST') => {
-  const config = {
-    method: method,
-    mode: "cors", // no-cors, cors, *same-origin
-    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: "same-origin", // include, *same-origin, omit
-    headers: { "Content-Type": "application/json; charset=utf-8" },
-    redirect: "follow", // manual, *follow, error
-    referrer: "no-referrer", // no-referrer, *client
-    body: JSON.stringify(data), // body data type must match "Content-Type" header
-  }
-  return fetch(url, config);
-}
-
-const apiPost = (url, data) => {
-  return apiRequest(url, data, 'POST');
-};
-
-class ApartmentsController extends Stimulus.Controller {
+class ApartmentsController extends AppController {
   static get targets() {
     return ["number", "code", "buildingId", "submitButton"];
   }
