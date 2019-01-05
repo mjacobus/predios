14542c257c6ff6b9303bd257031bd36b8e407f55
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-01-05 02:04:08 -0200

    Improve ui

--- lib/predios/entities/building_projection.rb
+++ lib/predios/entities/building_projection.rb
@@ -16,4 +16,9 @@ class BuildingProjection < Hanami::Entity
   def apartments
     super.sort_by(&:number)
   end
+
+  # rubocop:disable Naming/PredicateName
+  def has_all_apartments?
+    number_of_apartments >= apartments.length
+  end
 end
