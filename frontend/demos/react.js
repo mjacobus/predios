ac730d37fcef67e23e4db8b5739a4218fd634f07
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-01-18 15:58:06 -0200

    Add glamor

--- frontend/demos/react.js
+++ frontend/demos/react.js
@@ -1,10 +1,15 @@
 import React from "react";
 import ReactDom from "react-dom";
 import { Component } from "react";
+import { css } from "glamor";
+
+const red = css({
+  color: 'red'
+});
 
 class HelloWorld extends Component {
   render() {
-    return <h1>Hello World from react</h1>;
+    return <h1 className={ red }>Hello World from react</h1>;
   }
 }
 
