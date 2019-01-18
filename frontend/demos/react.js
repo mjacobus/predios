ffead4c15e170cd1f467cd89be4867039943d2b3
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-01-18 15:46:35 -0200

    First react component

--- frontend/demos/react.js
+++ frontend/demos/react.js
@@ -1,2 +1,11 @@
 import React from "react";
-console.log("react bootstrapp");
+import ReactDom from "react-dom";
+import { Component } from "react";
+
+class HelloWorld extends Component {
+  render() {
+    return <h1>Hello World from react</h1>;
+  }
+}
+
+ReactDom.render(<HelloWorld />, document.getElementById("app-container"));
