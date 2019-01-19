3656144a36de10ef5bd42b5d6653eb807e3e16a8
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-01-19 19:20:03 -0200

    Draft container

--- frontend/buildings.app.js
+++ frontend/buildings.app.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDom from "react-dom";
-import {BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from "react-router-dom";
+import BuildingsIndex from "./src/components/buildings/BuildingsIndexContainer";
 
-const Buildings = () => ( <h1>Buildings</h1> );
-const Apartments = () => ( <h1>Apartments</h1> );
+const Apartments = () => <h1>Apartments</h1>;
 
-const app = <BrowserRouter>
-  <div>
-    <Route exact path="/app/buildings" component={Buildings} />
-    <Route exact path="/app/buildings/:number/apartments" component={Apartments} />
-  </div>
-</BrowserRouter>
+const app = (
+  <BrowserRouter>
+    <div>
+      <Route exact path="/app/buildings" component={BuildingsIndex} />
+      <Route
+        exact
+        path="/app/buildings/:number/apartments"
+        component={Apartments}
+      />
+    </div>
+  </BrowserRouter>
+);
 
-ReactDom.render(app, document.getElementById('react-app'));
-
+ReactDom.render(app, document.getElementById("react-app"));
---
+++ frontend/src/components/buildings/BuildingsIndexContainer.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BuildingsIndexContainer() {
+  return <h1>Buildings Index \o/</h1>;
+}
