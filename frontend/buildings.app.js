e8c9dfe42d98893e11cf8929649e043030b113da
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-01-19 19:03:04 -0200

    Basic routing added

--- frontend/buildings.app.js
+++ frontend/buildings.app.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDom from "react-dom";
+import {BrowserRouter, Route} from 'react-router-dom';
 
-const app = <h1>Hello from react</h1>;
+const Buildings = () => ( <h1>Buildings</h1> );
+const Apartments = () => ( <h1>Apartments</h1> );
+
+const app = <BrowserRouter>
+  <div>
+    <Route exact path="/app/buildings" component={Buildings} />
+    <Route exact path="/app/buildings/:number/apartments" component={Apartments} />
+  </div>
+</BrowserRouter>
 
 ReactDom.render(app, document.getElementById('react-app'));
+
