6eaa10af405d54dc0c2b3642bb29743ad5934a1b
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-01-20 21:56:10 -0200

    Adjust top menu

--- frontend/src/components/library/TopMenu.js
+++ frontend/src/components/library/TopMenu.js
@@ -2,26 +2,26 @@ import React from "react";
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from "react-bootstrap";
 
 export default function AppNavbar() {
-  return <Navbar>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a href="#home">React-Bootstrap</a>
-      </Navbar.Brand>
-    </Navbar.Header>
-    <Nav>
-      <NavItem eventKey={1} href="#">
-        Link
-      </NavItem>
-      <NavItem eventKey={2} href="#">
-        Link
-      </NavItem>
-      <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-        <MenuItem eventKey={3.1}>Action</MenuItem>
-        <MenuItem eventKey={3.2}>Another action</MenuItem>
-        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-        <MenuItem divider />
-        <MenuItem eventKey={3.4}>Separated link</MenuItem>
-      </NavDropdown>
-    </Nav>
-  </Navbar>;
+  return (
+    <Navbar>
+      <Navbar.Header>
+        <Navbar.Brand>
+          <a href="/">Página inicial</a>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+      </Navbar.Header>
+      <Navbar.Collapse>
+        <Nav>
+          <NavItem eventKey={1} href="/buildings"> Prédios </NavItem>
+          <NavItem eventKey={2} href="/Users"> Usuários </NavItem>
+          <NavDropdown eventKey={3} title="Perfil" id="basic-nav-dropdown">
+            <MenuItem eventKey={3.2}>Template</MenuItem>
+            <MenuItem eventKey={3.3}>Template</MenuItem>
+            <MenuItem divider />
+            <MenuItem eventKey={3.1} href="/logout">Sair</MenuItem>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
