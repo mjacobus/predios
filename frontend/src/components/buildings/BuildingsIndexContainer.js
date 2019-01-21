dece2a81d6dbab5d0e06ec3063b1c64eef5827cf
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-01-20 22:14:10 -0200

    WIP apartments

--- frontend/src/components/buildings/BuildingsIndexContainer.js
+++ frontend/src/components/buildings/BuildingsIndexContainer.js
@@ -1,5 +1,29 @@
 import React from "react";
 
+const mockApartment = number => {
+  return {
+    name: `Nome ${number}`,
+    address: `The Address Foo Bar number ${number}`,
+    neighborhood: `Bairro ${number}`,
+    number: `Bairro ${number}`
+  };
+};
+
+const mockBuildings = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(i =>
+  mockApartment(i)
+);
+
+const Apartment = ({ apartment }) => <div>{apartment.name}</div>;
+
+const buildings = mockBuildings.map(apartment => (
+  <Apartment apartment={apartment} />
+));
+
 export default function BuildingsIndexContainer() {
-  return <h1>Buildings Index \o/</h1>;
+  return (
+    <div>
+      <h1>buildings</h1>
+      {buildings}
+    </div>
+  );
 }
