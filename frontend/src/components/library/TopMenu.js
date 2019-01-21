6fbb8a0534a43ebfc016aab9be4f31bd61040b5a
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-01-20 22:06:25 -0200

    Fixup

--- frontend/src/components/library/TopMenu.js
+++ frontend/src/components/library/TopMenu.js
@@ -12,13 +12,21 @@ export default function AppNavbar() {
       </Navbar.Header>
       <Navbar.Collapse>
         <Nav>
-          <NavItem eventKey={1} href="/buildings"> Prédios </NavItem>
-          <NavItem eventKey={2} href="/Users"> Usuários </NavItem>
+          <NavItem eventKey={1} href="/buildings">
+            {" "}
+            Prédios{" "}
+          </NavItem>
+          <NavItem eventKey={2} href="/Users">
+            {" "}
+            Usuários{" "}
+          </NavItem>
           <NavDropdown eventKey={3} title="Perfil" id="basic-nav-dropdown">
             <MenuItem eventKey={3.2}>Template</MenuItem>
             <MenuItem eventKey={3.3}>Template</MenuItem>
             <MenuItem divider />
-            <MenuItem eventKey={3.1} href="/logout">Sair</MenuItem>
+            <MenuItem eventKey={3.1} href="/logout">
+              Sair
+            </MenuItem>
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
