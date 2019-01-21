00a3438e9636af48874e47947bba9ae8164d2aa6
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-01-21 18:25:43 -0200

    Add basic grid view

--- frontend/buildings.app.js
+++ frontend/buildings.app.js
@@ -3,19 +3,31 @@ import ReactDom from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
 import TopMenu from "./src/components/library/TopMenu";
 import BuildingsIndex from "./src/components/buildings/BuildingsIndexContainer";
+import { Grid, Col, Row } from "react-bootstrap";
 
 const Apartments = () => <h1>Apartments</h1>;
+
+const MidContent = (props) => {
+  return <Grid>
+    <Row>
+      <Col mdOffset={2} sm={12} md={8}>{ props.children }</Col>
+    </Row>
+  </Grid>
+  { props.children }
+};
 
 const app = (
   <BrowserRouter>
     <div>
       <TopMenu />
-      <Route exact path="/app/buildings" component={BuildingsIndex} />
-      <Route
-        exact
-        path="/app/buildings/:number/apartments"
-        component={Apartments}
-      />
+      <MidContent>
+        <Route exact path="/app/buildings" component={BuildingsIndex} />
+        <Route
+          exact
+          path="/app/buildings/:number/apartments"
+          component={Apartments}
+        />
+      </MidContent>
     </div>
   </BrowserRouter>
 );
