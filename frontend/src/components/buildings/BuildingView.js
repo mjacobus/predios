a5e2737639683f009da0b866c242177a3d96e379
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-01-27 00:36:18 -0200

    Progress

--- frontend/src/components/buildings/BuildingView.js
+++ frontend/src/components/buildings/BuildingView.js
@@ -12,6 +12,23 @@ import {
   BuildingName
 } from "./index";
 
+const Apartment = (props) => {
+  const { apartment } = props;
+
+  return <Grid>
+        <Row>
+          <Col xs={2}>
+            {apartment.number}
+          </Col>
+          <Col xs={6}>
+          </Col>
+          <Col xs={4}>
+          </Col>
+        </Row>
+    </Grid>
+      
+}
+
 export default function BuildingView(props) {
   const { fetching, building } = props;
 
@@ -20,29 +37,34 @@ export default function BuildingView(props) {
   }
 
   return (
-    <Grid>
-      <Row>
-        <Col xs={2}>
-          <BuildingLink number={building.number}>
-            <ApartmentNumber>{building.number}</ApartmentNumber>
-          </BuildingLink>
-          <NumberOfApartments>
-            {building.number_of_apartments}
-          </NumberOfApartments>
-        </Col>
-        <Col xs={8}>
-          <BuildingLink number={building.number}>
-            <BuildingName>{building.name}</BuildingName>
-          </BuildingLink>
-          <BuildingLink number={building.number}>
-            <BuildingAddress>{building.address}</BuildingAddress>
-          </BuildingLink>
-          <Neighborhood>{building.neighborhood}</Neighborhood>
-        </Col>
-        <Col xs={2}>
-          <CallOptions options={building.call_options} />
-        </Col>
-      </Row>
-    </Grid>
+    <div>
+      <Grid>
+        <Row>
+          <Col xs={2}>
+            <BuildingLink number={building.number}>
+              <ApartmentNumber>{building.number}</ApartmentNumber>
+            </BuildingLink>
+            <NumberOfApartments>
+              {building.number_of_apartments}
+            </NumberOfApartments>
+          </Col>
+          <Col xs={8}>
+            <BuildingLink number={building.number}>
+              <BuildingName>{building.name}</BuildingName>
+            </BuildingLink>
+            <BuildingLink number={building.number}>
+              <BuildingAddress>{building.address}</BuildingAddress>
+            </BuildingLink>
+            <Neighborhood>{building.neighborhood}</Neighborhood>
+          </Col>
+          <Col xs={2}>
+            <CallOptions options={building.call_options} />
+          </Col>
+        </Row>
+      </Grid>
+      {building.apartments.map(a => (
+        <Apartment apartment={a} key={a.uuid} />
+      ))}
+    </div>
   );
 }
