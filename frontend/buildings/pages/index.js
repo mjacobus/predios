18a389c217253e51ddc9c271600ce9ce33c9dbb6
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-02-08 14:45:38 -0200

    Cleanup

--- frontend/buildings/pages/index.js
+++ frontend/buildings/pages/index.js
@@ -1,9 +1,7 @@
 import Show from "../../src/components/buildings/BuildingViewContainer";
-import List from "../../src/components/buildings/BuildingsIndexContainer";
 
 const Buildings = {
   Show,
-  List
 };
 
 export default Buildings;
