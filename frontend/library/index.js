33156f0acf68845157b82bfdb8b94fb2864c8d31
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-02-08 19:58:14 -0200

    Reorganize index

--- frontend/library/index.js
+++ frontend/library/index.js
@@ -1,21 +1,25 @@
-import React from "react";
-import { css } from "glamor";
-import { fontSizes, colors, styles } from "./styles";
-
-import Icon from "./Icon";
-export { Icon };
-
-import DefaultLayout from "./DefaultLayout";
-export { DefaultLayout };
-
-import Loader from "./Loader";
-import H1 from "./H1";
-import Label from "./Label";
 import A from "./A";
-import Input from "./Input";
 import Button from "./Button";
 import CheckIcon from "./CheckIcon";
-import Form from "./Form";
 import DateTime from "./DateTime";
+import DefaultLayout from "./DefaultLayout";
+import Form from "./Form";
+import H1 from "./H1";
+import Icon from "./Icon";
+import Input from "./Input";
+import Label from "./Label";
+import Loader from "./Loader";
 
-export { Loader, H1, Label, Input, A, Button, CheckIcon, DateTime, Form };
+export {
+  A,
+  Button,
+  CheckIcon,
+  DateTime,
+  DefaultLayout,
+  Form,
+  H1,
+  Icon,
+  Input,
+  Label,
+  Loader
+};
