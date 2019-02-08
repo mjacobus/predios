b33ffd90ef9c640679384c6b84db4949dbeb7a6e
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-02-08 19:01:18 -0200

    Extract CallOptions

--- frontend/buildings/components/index.js
+++ frontend/buildings/components/index.js
@@ -12,6 +12,7 @@ import Neighborhood from "./Neighborhood";
 import BuildingName from "./BuildingName";
 import BuildingAddress from "./BuildingAddress";
 import BuildingLink from "./BuildingAddress";
+import CallOptions from "./CallOptions";
 
 export {
   ApartmentForm,
@@ -21,30 +22,8 @@ export {
   Neighborhood,
   BuildingAddress,
   BuildingLink,
+  CallOptions,
   BuildingName
-};
-
-// phone, letter, intercom
-export const CallOptions = props => {
-  const { options } = props;
-  let elements = [];
-  const className = css({
-    float: "right"
-  });
-
-  if (options.indexOf("intercom") >= 0) {
-    elements.push(<Icon type="building" key="1" />);
-  }
-
-  if (options.indexOf("letter") >= 0) {
-    elements.push(<Icon type="envelope" key="2" />);
-  }
-
-  if (options.indexOf("phone") >= 0) {
-    elements.push(<Icon type="phone" key="3" />);
-  }
-
-  return <span className={className}>{elements}</span>;
 };
 
 export const ContactAttempt = props => {
