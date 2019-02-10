c16dec0219b0ec879c185ece6e22736292877d1e
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-02-10 14:33:45 -0200

    Improve container

--- frontend/buildings/pages/ListContainer.js
+++ frontend/buildings/pages/ListContainer.js
@@ -2,6 +2,10 @@ import React from "react";
 import { connect } from "react-redux";
 import List from "./List";
 import { fetchBuildings } from "../actions";
+import { Loader } from "../../library";
+import Filter from "./list/FilterContainer";
+import Building from "./list/Building";
+import { H1 } from "../../library";
 
 function mapStateToProps(state) {
   return {
@@ -29,7 +33,19 @@ class BuildingsContainer extends React.Component {
 
   render() {
     const buildings = this.props.filteredBuildings;
-    return <List buildings={buildings} />;
+    const { filter, fetching } = this.props;
+
+    return (
+      <div>
+        <H1>Edifícios</H1>
+        <Filter />
+        {fetching && <Loader />}
+        {fetching ||
+          buildings.map((building, i) => (
+            <Building building={building} key={i} />
+          ))}
+      </div>
+    );
   }
 }
 
