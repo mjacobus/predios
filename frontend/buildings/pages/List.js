69f9750bcf0648f2d5c25e32f6998d4502058581
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-02-10 12:28:22 -0200

    Extract Building to its own file

--- frontend/buildings/pages/List.js
+++ frontend/buildings/pages/List.js
@@ -4,51 +4,7 @@ import { css } from "glamor";
 import { H1 } from "../../library";
 import { Loader } from "../../library";
 import Filter from "./list/Filter";
-import {
-  BuildingNumber,
-  NumberOfApartments,
-  BuildingAddress,
-  Neighborhood,
-  BuildingLink,
-  CallOptions,
-  BuildingName
-} from "../components";
-
-const Building = ({ building }) => {
-  const buildingClass = css({
-    borderBottom: "1px solid #dddddd",
-    padding: "10px 0"
-  });
-
-  return (
-    <div className={buildingClass}>
-      <Grid>
-        <Row>
-          <Col xs={2}>
-            <BuildingLink number={building.number}>
-              <BuildingNumber>{building.number}</BuildingNumber>
-            </BuildingLink>
-            <NumberOfApartments>
-              {building.number_of_apartments}
-            </NumberOfApartments>
-          </Col>
-          <Col xs={8}>
-            <BuildingLink number={building.number}>
-              <BuildingName>{building.name}</BuildingName>
-            </BuildingLink>
-            <BuildingLink number={building.number}>
-              <BuildingAddress>{building.address}</BuildingAddress>
-            </BuildingLink>
-            <Neighborhood>{building.neighborhood}</Neighborhood>
-          </Col>
-          <Col xs={2}>
-            <CallOptions options={building.call_options} />
-          </Col>
-        </Row>
-      </Grid>
-    </div>
-  );
-};
+import Building from "./list/Building";
 
 export default function BuildingsIndex(props) {
   const { filter, buildings, currentUser, fetching } = props;
---
+++ frontend/buildings/pages/list/Building.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Grid, Col, Row } from "react-bootstrap";
+import { css } from "glamor";
+import {
+  BuildingNumber,
+  NumberOfApartments,
+  BuildingAddress,
+  Neighborhood,
+  BuildingLink,
+  CallOptions,
+  BuildingName
+} from "../../components";
+
+export default function Building({ building }) {
+  const buildingClass = css({
+    borderBottom: "1px solid #dddddd",
+    padding: "10px 0"
+  });
+
+  return (
+    <div className={buildingClass}>
+      <Grid>
+        <Row>
+          <Col xs={2}>
+            <BuildingLink number={building.number}>
+              <BuildingNumber>{building.number}</BuildingNumber>
+            </BuildingLink>
+            <NumberOfApartments>
+              {building.number_of_apartments}
+            </NumberOfApartments>
+          </Col>
+          <Col xs={8}>
+            <BuildingLink number={building.number}>
+              <BuildingName>{building.name}</BuildingName>
+            </BuildingLink>
+            <BuildingLink number={building.number}>
+              <BuildingAddress>{building.address}</BuildingAddress>
+            </BuildingLink>
+            <Neighborhood>{building.neighborhood}</Neighborhood>
+          </Col>
+          <Col xs={2}>
+            <CallOptions options={building.call_options} />
+          </Col>
+        </Row>
+      </Grid>
+    </div>
+  );
+}
