1522826c930f2f90b91198cc9ec10a856fae431e
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-02-15 18:05:46 -0200

    WIP

--- frontend/buildings/pages/show/BuildingHeader.js
+++ frontend/buildings/pages/show/BuildingHeader.js
@@ -8,7 +8,7 @@ import {
   CallOptions,
   Neighborhood,
   NumberOfApartments
-} from "../../components";
+} from "../../../shared/components";
 
 export default function BuildingHeader({ building }) {
   return (
