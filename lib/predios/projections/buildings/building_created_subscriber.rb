830dedf0d62af7678ad30de756b919a5df554b4e
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-03-10 18:05:31 -0300

    Fix subscriber

--- lib/predios/projections/buildings/building_created_subscriber.rb
+++ lib/predios/projections/buildings/building_created_subscriber.rb
@@ -3,20 +3,18 @@
 module Buildings
   class BuildingCreatedSubscriber
     def publish(event)
-      event.payload.symbolize.tap do |payload|
-        entity = BuildingProjection.new(
-          uuid: event.aggregate_id,
-          address: payload.fetch(:address),
-          number: payload.fetch(:number),
-          number_of_apartments: payload.fetch(:number_of_apartments),
-          name: payload.fetch(:name),
-          neighborhood: payload.fetch(:neighborhood),
-          has_individual_letterboxes: payload.fetch(:has_individual_letterboxes),
-          has_individual_intercoms: payload.fetch(:has_individual_intercoms)
-        )
+      entity = BuildingProjection.new(
+        uuid: event.aggregate_id,
+        address: event.address,
+        number: event.number,
+        number_of_apartments: event.number_of_apartments,
+        name: event.name,
+        neighborhood: event.neighborhood,
+        has_individual_letterboxes: event.has_individual_letterboxes,
+        has_individual_intercoms: event.has_individual_intercoms
+      )
 
-        repository.save(entity)
-      end
+      repository.save(entity)
     end
 
     private
