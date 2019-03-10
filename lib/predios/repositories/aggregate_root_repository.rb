3da3c3e7940e6d17501da3da0b0fcc1685ca6d19
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-03-10 17:35:51 -0300

    Only save items in a transaction

--- lib/predios/repositories/aggregate_root_repository.rb
+++ lib/predios/repositories/aggregate_root_repository.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class AggregateRootRepository < Koine::EventSourcing::AggregateRootRepository
+  def save(*args)
+    transaction do
+      super(*args)
+    end
+  end
+
+  private
+
+  def transaction(&block)
+    UserRepository.new.transaction(&block)
+  end
 end
