96134cb2cd26347b6ed199ec5226eeb8f84b6a07
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-04-04 15:31:19 -0300

    Add rake task

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,3 +7,7 @@ end
 every 1.day, at: ['5:30 am'], roles: [:mysql_dump] do
   rake 'email_backup'
 end
+
+every 1.day, at: ['5:00 am'], roles: [:mysql_dump] do
+  rake "geolocation:update[#{ENV.fetch('CITY_NAME')}]"
+end
