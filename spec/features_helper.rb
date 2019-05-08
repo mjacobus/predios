5bf138c578662a0a953fb15483236c4faaa79d3a
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-05-08 14:37:44 -0300

    Attempt to fix chrome driver

--- spec/features_helper.rb
+++ spec/features_helper.rb
@@ -4,6 +4,9 @@
 require_relative './spec_helper'
 require 'webdrivers'
 
+# Chrome
+Webdrivers::Chromedriver.required_version = '2.46'
+
 RSpec.configure do |config|
   config.include FeatureHelpers, type: :feature
 end
