b22c6a7ac6033d0c8da7a055ef8ac1d8ef80bd96
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-05-08 14:27:32 -0300

    First incomplete working spec

--- spec/feature/buildings/create_spec.rb
+++ spec/feature/buildings/create_spec.rb
@@ -4,7 +4,10 @@ require 'features_helper'
 
 RSpec.describe 'creating buildings', type: :feature do
   it 'does something' do
-    browser.goto(url)
-    sleep 3
+    login_as_master
+    visit('/buildings/new')
+    expect(page).to have_content('Criação de Prédio')
+
+    # wip
   end
 end
--- spec/support/feature_helpers.rb
+++ spec/support/feature_helpers.rb
@@ -8,8 +8,11 @@ module FeatureHelpers
         BrowserFactory::Options.from_cli_options(string)
       end
 
-      let(:browser) do
-        BrowserFactory.new.from_options(browser_options)
+      let(:browser) { BrowserFactory.new.from_options(browser_options) }
+      let(:page) { CurrentTestPage.new(browser) }
+
+      before do
+        UserRepository.new.clear
       end
 
       after do
@@ -20,5 +23,43 @@ module FeatureHelpers
 
   def url(path = '/')
     "http://localhost:4000#{path}"
+  end
+
+  def visit(path)
+    browser.goto(url(path))
+  end
+
+  def login_as_master
+    user = create_user do |u|
+      u.as_master.enable
+    end
+
+    login_as(user)
+  end
+
+  def login
+    login_as(create_user {})
+  end
+
+  def login_as(user)
+    visit("/dev/login?user_id=#{user.id}")
+  end
+
+  def create_user
+    args = {
+      name: 'John Doe',
+      email: 'jon@doe.com',
+      oauth_provider: 'google',
+      oauth_uid: 'someid',
+      enabled: true,
+    }
+
+    user = User.new(args)
+
+    if block_given?
+      user = yield(user)
+    end
+
+    UserRepository.new.create(user)
   end
 end
