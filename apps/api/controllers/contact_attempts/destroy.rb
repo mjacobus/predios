e01cc949004c1e86511177954162397d515fb7dc
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2019-05-30 21:55:52 +0200

    Progress

--- apps/api/controllers/contact_attempts/destroy.rb
+++ apps/api/controllers/contact_attempts/destroy.rb
@@ -7,13 +7,19 @@ module Api
         private
 
         def safe_call(params)
-          payload = params[:contact_attempts]
-          # command = ::Apartments::Commands::AssignContactAttempt.new(payload)
-          # execute(command)
-          render(status: 201, body: payload)
+          commands = params[:contact_attempts].map do |attempt|
+            ::Apartments::Commands::UnassignContactAttempt.new(
+              attempt.merge(apartment_uuid: params[:apartment_id])
+            )
+          end
+
+          commands.each do |command|
+            execute(command)
+          end
+
+          render(status: 201)
         end
       end
     end
   end
 end
-
--- spec/api/controllers/contact_attempts/destroy_spec.rb
+++ spec/api/controllers/contact_attempts/destroy_spec.rb
@@ -2,14 +2,30 @@
 
 require 'spec_helper'
 
-RSpec.describe Api::Controllers::ContactAttempts::Delete, type: :action do
-  let(:params) { Hash[contact_attempts: contact_attempts] }
-  let(:contact_attempts) do
-    []
+RSpec.describe Api::Controllers::ContactAttempts::Destroy, type: :action do
+  let(:current_user) { active_user }
+  let(:params) do
+    {
+      apartment_id: 'apartment-uuid',
+      contact_attempts: [{
+        time: 'the-time',
+        type: 'the-type',
+        outcome: 'the-outcome',
+      }],
+    }
   end
+
+  let(:command) { instance_double(Apartments::Commands::UnassignContactAttempt) }
 
   before do
     allow(action).to receive(:execute)
+
+    allow(Apartments::Commands::UnassignContactAttempt).to receive(:new).with(
+      apartment_uuid: 'apartment-uuid',
+      time: 'the-time',
+      outcome: 'the-outcome',
+      type: 'the-type'
+    ).and_return(command)
   end
 
   it 'has proper superclass' do
@@ -17,13 +33,12 @@ RSpec.describe Api::Controllers::ContactAttempts::Delete, type: :action do
   end
 
   it 'executes proper action' do
-    # response
-    #
-    # expected_command = Apartments::Commands::AssignContactAttempt.new(contact_attempt)
-    #
-    # expect(action).to have_received(:execute) do |command|
-    #   expect(command).to be_equal_to(expected_command)
-    # end
+    response
+
+    expect(action).to have_received(:execute).with(command)
+  end
+
+  it 'responds with correct status code' do
+    expect(unsafe_response).to be_successful
   end
 end
-
